08a3192f1f279d9dfc9637f9d4e6ab752b325dd0
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-15 14:32:53 +0100

    Initial Window creation

--- RTSClone/RTSClone/main.cpp
+++ RTSClone/RTSClone/main.cpp
@@ -1,8 +1,36 @@
+#include "glm/glm.hpp"
+#include "glad.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World\n";
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 4;
+	settings.majorVersion = 3;
+	settings.minorVersion = 3;
+	settings.attributeFlags = sf::ContextSettings::Core;
+	glm::uvec2 windowSize(640, 480);
+	sf::Window window(sf::VideoMode(windowSize.x, windowSize.y), "RTS Clone", sf::Style::Default, settings);
+	window.setFramerateLimit(60);
+	gladLoadGL();
+
+
+	glViewport(0, 0, windowSize.x, windowSize.y);
+
+	while (window.isOpen())
+	{
+		sf::Event currentSFMLEvent;
+		while (window.pollEvent(currentSFMLEvent))
+		{
+			if (currentSFMLEvent.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
 
 	return 0;
 }
