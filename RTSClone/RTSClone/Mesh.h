f5a16f64c71c420dc9aa7c68e1b62cb12d286736
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-15 18:28:06 +0100

    Assimp Implementation

---
+++ RTSClone/RTSClone/Mesh.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <vector>
+
+struct Vertex 
+{
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+};
+
+struct Mesh
+{
+	
+};
