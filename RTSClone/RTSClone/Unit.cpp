dd57f82960f141c71de10bc2236528a63bbaf02a
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-27 19:06:40 +0100

    Unit now deriving from Entity

--- RTSClone/RTSClone/Unit.cpp
+++ RTSClone/RTSClone/Unit.cpp
@@ -1,8 +1,7 @@
 #include "Unit.h"
 #include "ShaderHandler.h"
 #include "Model.h"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtx/transform.hpp"
+
 #include "PathFinding.h"
 
 namespace
@@ -66,27 +65,10 @@ namespace
 }
 
 Unit::Unit(const glm::vec3& startingPosition)
-	: m_position(startingPosition), 
+	: Entity(startingPosition),
 	m_front(),
-	m_AABB(startingPosition, 5.0f),
-	m_selected(false),
 	m_pathToPosition()
 {}
-
-const AABB& Unit::getAABB() const
-{
-	return m_AABB;
-}
-
-bool Unit::isSelected() const
-{
-	return m_selected;
-}
-
-void Unit::setSelected(bool selected)
-{
-	m_selected = selected;
-}
 
 void Unit::moveTo(const glm::vec3& destinationPosition)
 {
@@ -110,14 +92,6 @@ void Unit::update(float deltaTime)
 			m_pathToPosition.pop_back();
 		}
 	}
-}
-
-void Unit::render(ShaderHandler& shaderHandler, const Model& renderModel) const
-{
-	glm::mat4 model = glm::scale(glm::mat4(1.0), glm::vec3(1.0f, 1.0f, 1.0f));
-	model = glm::translate(model, m_position);
-	shaderHandler.setUniformMat4f(eShaderType::Default, "uModel", model);
-	renderModel.render(shaderHandler, m_selected);
 }
 
 #ifdef RENDER_PATHING
--- RTSClone/RTSClone/Unit.h
+++ RTSClone/RTSClone/Unit.h
@@ -1,36 +1,27 @@
 #pragma once
 
-#include "glm/glm.hpp"
-#include "AABB.h"
+#include "Entity.h"
 #include <vector>
-
 #ifdef RENDER_PATHING
 #include "Mesh.h"
 #endif // RENDER_PATHING
 
 class ShaderHandler;
 struct Model;
-class Unit
+class Unit : public Entity
 {
 public:
 	Unit(const glm::vec3& startingPosition);
 
-	const AABB& getAABB() const;
-	bool isSelected() const;
-	
-	void setSelected(bool selected);
 	void moveTo(const glm::vec3& destinationPosition);
-	void update(float deltaTime);
-	void render(ShaderHandler& shaderHandler, const Model& renderModel) const;
+	void update(float deltaTime) override;
+
 #ifdef RENDER_PATHING
 	void renderPathMesh(ShaderHandler& shaderHandler);
 #endif // RENDER_PATHING
 
 private:
-	glm::vec3 m_position;
 	glm::vec3 m_front;
-	AABB m_AABB;
-	bool m_selected;
 	std::vector<glm::vec3> m_pathToPosition;
 
 #ifdef RENDER_PATHING
