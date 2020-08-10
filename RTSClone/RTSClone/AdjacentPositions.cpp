f8b495fe93d896e0c670e3aaefe634c1ea03be45
Ryan Swann <RyanSwann1992@gmail.com>
2020-08-10 20:01:32 +0100

    New File - Adjacent positions
    Moved out of PathFinding.cpp

---
+++ RTSClone/RTSClone/AdjacentPositions.cpp
@@ -0,0 +1,133 @@
+#include "AdjacentPositions.h"
+#include "Map.h"
+#include "Globals.h"
+#include "Unit.h"
+
+AdjacentPosition::AdjacentPosition()
+	: valid(false),
+	unitCollision(false),
+	position()
+{}
+
+AdjacentPosition::AdjacentPosition(const glm::ivec2 & position)
+	: valid(true),
+	unitCollision(false),
+	position(position)
+{}
+
+AdjacentPosition::AdjacentPosition(const glm::ivec2 & position, bool valid, bool unitCollision)
+	: valid(valid),
+	unitCollision(unitCollision),
+	position(position)
+{}
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2 & position, const Map & map, const std::vector<Unit> & units)
+{
+	std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> adjacentPositions;
+	for (int i = 0; i < adjacentPositions.size(); ++i)
+	{
+		glm::ivec2 adjacentPosition = position + ALL_DIRECTIONS_ON_GRID[i];
+		if (Globals::isPositionInMapBounds(adjacentPosition) && !map.isPositionOccupied(adjacentPosition))
+		{
+			bool unitCollision = false;
+			for (const auto& otherUnit : units)
+			{
+				if (otherUnit.getAABB().contains(Globals::convertToWorldPosition(adjacentPosition)))
+				{
+					unitCollision = true;
+					break;
+				}
+			}
+
+			if (!unitCollision)
+			{
+				adjacentPositions[i] = AdjacentPosition(adjacentPosition, true, unitCollision);
+			}
+			else
+			{
+				adjacentPositions[i] = AdjacentPosition(adjacentPosition, true, unitCollision);
+			}
+		}
+	}
+
+	return adjacentPositions;
+}
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map, const std::vector<Unit>& units, const Unit& unit)
+{
+	std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> adjacentPositions;
+	for (int i = 0; i < adjacentPositions.size(); ++i)
+	{
+		glm::ivec2 adjacentPosition = position + ALL_DIRECTIONS_ON_GRID[i];
+		if (Globals::isPositionInMapBounds(adjacentPosition) && !map.isPositionOccupied(adjacentPosition))
+		{
+			bool unitCollision = false;
+			for (const auto& otherUnit : units)
+			{
+				if (&otherUnit != &unit && otherUnit.getCurrentState() == eUnitState::Idle)
+				{
+					if (otherUnit.getAABB().contains(Globals::convertToWorldPosition(adjacentPosition)))
+					{
+						unitCollision = true;
+						break;
+					}
+				}
+			}
+
+			if (!unitCollision)
+			{
+				adjacentPositions[i] = AdjacentPosition(adjacentPosition);
+			}
+		}
+	}
+
+	return adjacentPositions;
+}
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map, const std::vector<Unit>& units, const Unit& unit, const std::vector<const Unit*>& selectedUnits)
+{
+	std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> adjacentPositions;
+	for (int i = 0; i < adjacentPositions.size(); ++i)
+	{
+		glm::ivec2 adjacentPosition = position + ALL_DIRECTIONS_ON_GRID[i];
+		if (Globals::isPositionInMapBounds(adjacentPosition) && !map.isPositionOccupied(adjacentPosition))
+		{
+			bool unitCollision = false;
+			for (const auto& otherUnit : units)
+			{
+				if (&otherUnit != &unit &&
+					std::find(selectedUnits.cbegin(), selectedUnits.cend(), &otherUnit) == selectedUnits.cend() &&
+					otherUnit.getCurrentState() == eUnitState::Idle)
+				{
+					if (otherUnit.getAABB().contains(Globals::convertToWorldPosition(adjacentPosition)))
+					{
+						unitCollision = true;
+						break;
+					}
+				}
+			}
+
+			if (!unitCollision)
+			{
+				adjacentPositions[i] = AdjacentPosition(adjacentPosition);
+			}
+		}
+	}
+
+	return adjacentPositions;
+}
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map)
+{
+	std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> adjacentPositions;
+	for (int i = 0; i < adjacentPositions.size(); ++i)
+	{
+		glm::ivec2 adjacentPosition = position + ALL_DIRECTIONS_ON_GRID[i];
+		if (Globals::isPositionInMapBounds(adjacentPosition) && !map.isPositionOccupied(adjacentPosition))
+		{
+			adjacentPositions[i] = AdjacentPosition(adjacentPosition);
+		}
+	}
+
+	return adjacentPositions;
+}
---
+++ RTSClone/RTSClone/AdjacentPositions.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <array>
+#include <vector>
+
+constexpr std::array<glm::ivec2, 8> ALL_DIRECTIONS_ON_GRID =
+{
+	glm::ivec2(0, 1),
+	glm::ivec2(1, 1),
+	glm::ivec2(1, 0),
+	glm::ivec2(1, -1),
+	glm::ivec2(0, -1),
+	glm::ivec2(-1, -1),
+	glm::ivec2(-1, 0),
+	glm::ivec2(-1, 1)
+};
+
+struct AdjacentPosition
+{
+	AdjacentPosition();
+	AdjacentPosition(const glm::ivec2& position);
+	AdjacentPosition(const glm::ivec2& position, bool valid, bool unitCollision);
+
+	bool valid;
+	bool unitCollision;
+	glm::ivec2 position;
+};
+
+class Map;
+class Unit;
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map,
+	const std::vector<Unit>& units);
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map,
+	const std::vector<Unit>& units, const Unit& unit);
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map,
+	const std::vector<Unit>& units, const Unit& unit, const std::vector<const Unit*>& selectedUnits);
+
+std::array<AdjacentPosition, ALL_DIRECTIONS_ON_GRID.size()> getAllAdjacentPositions(const glm::ivec2& position, const Map& map);
