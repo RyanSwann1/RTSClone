394e6d43ca023e12799ad54d4766d62b66b3becb
Ryan Swann <RyanSwann1992@gmail.com>
2020-08-19 14:09:49 +0100

    New File GameEventMessenger

---
+++ RTSClone/RTSClone/GameEventMessenger.cpp
@@ -0,0 +1,46 @@
+#include "GameEventMessenger.h"
+
+Listener::Listener(const std::function<void(const void*)>& fp, const void* ownerAddress)
+	: m_listener(fp),
+	m_ownerAddress(ownerAddress)
+{
+	assert(ownerAddress != nullptr);
+}
+
+Listener::Listener(Listener&& orig) noexcept
+	: m_listener(orig.m_listener),
+	m_ownerAddress(orig.m_ownerAddress)
+{
+	orig.m_listener = nullptr;
+	orig.m_ownerAddress = nullptr;
+}
+
+Listener& Listener::operator=(Listener&& orig) noexcept
+{
+	m_listener = orig.m_listener;
+	m_ownerAddress = orig.m_ownerAddress;
+
+	orig.m_listener = nullptr;
+	orig.m_ownerAddress = nullptr;
+
+	return *this;
+}
+
+bool GameEventMessenger::isOwnerAlreadyRegistered(const std::vector<Listener>& listeners, eGameEventType gameEventType, const void* ownerAddress) const
+{
+	assert(ownerAddress != nullptr);
+
+	if (!listeners.empty())
+	{
+		auto result = std::find_if(listeners.cbegin(), listeners.cend(), [ownerAddress](const auto& listener)
+		{
+			return listener.m_ownerAddress == ownerAddress;
+		});
+
+		return result != listeners.cend();
+	}
+	else
+	{
+		return false;
+	}
+}
---
+++ RTSClone/RTSClone/GameEventMessenger.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "NonCopyable.h"
+#include "NonMovable.h"
+#include "GameEventType.h"
+#include <functional>
+#include <vector>
+#include <assert.h>
+#include <array>
+
+struct Listener : private NonCopyable
+{
+	Listener(const std::function<void(const void*)>& fp, const void* ownerAddress);
+	Listener(Listener&&) noexcept;
+	Listener& operator=(Listener&&) noexcept;
+
+	std::function<void(const void*)> m_listener;
+	const void* m_ownerAddress;
+};
+
+class GameEventMessenger : private NonCopyable, private NonMovable
+{
+public:
+	static GameEventMessenger& getInstance()
+	{
+		static GameEventMessenger instance;
+		return instance;
+	}
+
+	template <typename GameEvent>
+	void subscribe(const std::function<void(const GameEvent&)>& gameEvent, const void* ownerAddress)
+	{
+		auto& listeners = m_listeners[static_cast<int>(GameEvent::getType())];
+		assert(!isOwnerAlreadyRegistered(listeners, GameEvent::getType(), ownerAddress));
+
+		listeners.emplace_back(reinterpret_cast<std::function<void(const void*)> const&>(gameEvent), ownerAddress);
+	}
+
+	template <typename GameEvent>
+	void unsubscribe(const void* ownerAddress)
+	{
+		auto& listeners = m_listeners[static_cast<int>(GameEvent::getType())];
+		assert(isOwnerAlreadyRegistered(listeners, GameEvent::getType(), ownerAddress));
+
+		auto iter = std::find_if(listeners.begin(), listeners.end(), [ownerAddress](const auto& listener)
+		{
+			return listener.m_ownerAddress == ownerAddress;
+		});
+
+		assert(iter != listeners.end());
+		listeners.erase(iter);
+	}
+
+	template <typename GameEvent>
+	void broadcast(GameEvent gameEvent)
+	{
+		const auto& listeners = m_listeners[static_cast<int>(GameEvent::getType())];
+		for (const auto& listener : listeners)
+		{
+			reinterpret_cast<std::function<void(const GameEvent&)> const&>(listener.m_listener)(gameEvent);
+		}
+	}
+
+private:
+	GameEventMessenger() {}
+	std::array<std::vector<Listener>, static_cast<size_t>(eGameEventType::Max) + 1> m_listeners;
+
+	bool isOwnerAlreadyRegistered(const std::vector<Listener>& listeners, eGameEventType gameEventType, const void* ownerAddress) const;
+};
