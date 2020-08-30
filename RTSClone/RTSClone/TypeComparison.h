dbaa8b466e193c1ac85a9187e0f63e8348a2455d
Ryan Swann <RyanSwann1992@gmail.com>
2020-08-30 16:00:47 +0100

    Added new file - TypeComparison
    Compares enum types for match

---
+++ RTSClone/RTSClone/TypeComparison.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "EntityType.h"
+#include "NonCopyable.h"
+#include "NonMovable.h"
+#include <bitset>
+#include <vector>
+
+template <class Type>
+class TypeComparison : private NonCopyable, private NonMovable
+{
+public:
+	TypeComparison(const std::vector<Type>& cubeTypes)
+	{
+		for (auto cubeType : cubeTypes)
+		{
+			bitset.set(static_cast<int>(cubeType));
+		}
+	}
+
+	const std::bitset<static_cast<int>(Type::Max) + 1>& getBitSet() const
+	{
+		return bitset;
+	}
+
+	bool isMatch(Type cubeType) const
+	{
+		std::bitset<static_cast<int>(Type::Max) + 1> otherBitSet;
+		otherBitSet.set(static_cast<int>(cubeType));
+
+		return (bitset & otherBitSet).any();
+	}
+
+	bool isMatch(const TypeComparison& other) const
+	{
+		return (bitset & other.getBitSet()).any();
+	}
+
+private:
+	std::bitset<static_cast<int>(Type::Max) + 1> bitset{ 0 };
+};
