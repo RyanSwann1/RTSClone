f3c665fdd7d07bf24434bb08151bbe617b5a7d66
Ryan Swann <RyanSwann1992@gmail.com>
2020-09-09 11:49:22 +0100

    Added file - Selection Box
    Selection Box now shareable between both projects

---
+++ RTSClone/Shared/SelectionBox.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "NonCopyable.h"
+#include "NonMovable.h"
+#include "AABB.h"
+#include <SFML/Graphics.hpp>
+
+struct SelectionBox : private NonCopyable, private NonMovable
+{
+	SelectionBox();
+	~SelectionBox();
+
+	void setStartingPosition(const sf::Window& window, const glm::vec3& position);
+	void setSize(const glm::vec3& position);
+	void reset();
+	void render(const sf::Window& window) const;
+
+	AABB AABB;
+	bool active;
+	glm::vec3 mouseToGroundPosition;
+	glm::vec2 startingPositionScreenPosition;
+	glm::vec3 startingPositionWorldPosition;
+
+private:
+	unsigned int vaoID;
+	unsigned int vboID;
+};
