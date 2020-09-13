205197dd1120080fd82ea09289f58ae1fe17e6e2
Ryan Swann <RyanSwann1992@gmail.com>
2020-09-13 16:51:46 +0100

    Added overloaded << operator to load into ostream

--- RTSClone/LevelEditor/Player.cpp
+++ RTSClone/LevelEditor/Player.cpp
@@ -1,11 +1,13 @@
 #include "Player.h"
+#include <sstream>
 
-Player::Player(const glm::vec3& startingHQPosition, const glm::vec3& startingMineralPosition)
-	: m_minerals(),
-	m_HQ(eModelName::HQ, Globals::convertToNodePosition(startingHQPosition))
+Player::Player(ePlayerType playerType, const glm::vec3& startingHQPosition, const glm::vec3& startingMineralPosition)
+	: type(playerType),
+	HQ(eModelName::HQ, Globals::convertToNodePosition(startingHQPosition)),
+	minerals()
 {
 	glm::vec3 mineralSpawnPosition = Globals::convertToNodePosition(startingMineralPosition);
-	for(auto& mineral : m_minerals)
+	for(auto& mineral : minerals)
 	{
 		mineral.setModelName(eModelName::Mineral);
 		mineral.setPosition(mineralSpawnPosition);
@@ -15,10 +17,36 @@ Player::Player(const glm::vec3& startingHQPosition, const glm::vec3& startingMin
 
 void Player::render(ShaderHandler& shaderHandler) const
 {
-	for (const auto& mineral : m_minerals)
+	for (const auto& mineral : minerals)
 	{
 		mineral.render(shaderHandler);
 	}
 
-	m_HQ.render(shaderHandler);
+	HQ.render(shaderHandler);
+}
+
+std::ostream& operator<<(std::ostream& ostream, const Player& player)
+{
+	switch (player.type)
+	{
+	case ePlayerType::Human:
+		ostream << Globals::TEXT_HEADER_PLAYER;
+		break;
+	case ePlayerType::AI:
+		ostream << Globals::TEXT_HEADER_PLAYERAI;
+		break;
+	default:
+		assert(false);
+	}
+
+	ostream << Globals::TEXT_HEADER_HQ;
+	ostream << player.HQ.getPosition().x << " " << player.HQ.getPosition().y << " " << player.HQ.getPosition().z << "\n";
+
+	ostream << Globals::TEXT_HEADER_MINERALS;
+	for (const auto& mineral : player.minerals)
+	{
+		ostream << mineral.getPosition().x << " " << mineral.getPosition().y << " " << mineral.getPosition().z << "\n";
+	}
+
+	return ostream;
 }
--- RTSClone/LevelEditor/Player.h
+++ RTSClone/LevelEditor/Player.h
@@ -2,14 +2,20 @@
 
 #include "Entity.h"
 #include "Globals.h"
+#include "PlayerType.h"
 #include <array>
+#include <string>
+#include <ostream>
 
 struct Player
 {
-	Player(const glm::vec3& startingHQPosition, const glm::vec3& startingMineralPosition);
+	Player(ePlayerType playerType, const glm::vec3& startingHQPosition, const glm::vec3& startingMineralPosition);
 
 	void render(ShaderHandler& shaderHandler) const;
+	
+	friend std::ostream& operator<<(std::ostream& ostream, const Player& player);
 
-	std::array<Entity, Globals::MAX_MINERALS_PER_FACTION> m_minerals;
-	Entity m_HQ;
+	const ePlayerType type;
+	Entity HQ;
+	std::array<Entity, Globals::MAX_MINERALS_PER_FACTION> minerals;
 };
