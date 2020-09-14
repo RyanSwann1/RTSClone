924c665128546cefa8c21f344ce03a2d387faa07
Ryan Swann <RyanSwann1992@gmail.com>
2020-09-14 22:58:17 +0100

    Changed reading from file to updated version

--- RTSClone/LevelEditor/Player.cpp
+++ RTSClone/LevelEditor/Player.cpp
@@ -1,6 +1,8 @@
 #include "Player.h"
+#include "LevelFileHandler.h"
 #include <sstream>
 #include <fstream>
+#include <functional>
 
 Player::Player(ePlayerType playerType, const glm::vec3& startingHQPosition, const glm::vec3& startingMineralPosition)
 	: type(playerType),
@@ -28,47 +30,37 @@ void Player::render(ShaderHandler& shaderHandler) const
 
 const std::ifstream& operator>>(std::ifstream& file, Player& player)
 {
-	assert(file.is_open());
-	bool beginReadingFromFile = false;
-	std::string line;
-	while (getline(file, line))
+	auto data = [&player](const std::string& line)
 	{
-		if (beginReadingFromFile)
+		std::stringstream stream{ line };
+		std::string rawModelName;
+		glm::vec3 position;
+		stream >> rawModelName >> position.x >> position.y >> position.z;
+		switch (static_cast<eModelName>(std::stoi(rawModelName)))
 		{
-			if (line[0] == *Globals::TEXT_HEADER_BEGINNING.c_str())
-			{
-				file.clear();
-				file.seekg(0);
-				break;
-			}
-
-			std::stringstream stream{ line };
-			std::string rawModelName;
-			glm::vec3 position;
-			stream >> rawModelName >> position.x >> position.y >> position.z;
-			switch(static_cast<eModelName>(std::stoi(rawModelName)))
-			{
-			case eModelName::HQ:
-				player.HQ.setPosition(position);
-				break;
-			case eModelName::Mineral:
-			{
-				int mineralIndex = -1;
-				stream >> mineralIndex;
-				assert(mineralIndex >= 0 && mineralIndex < player.minerals.size());
-
-				player.minerals[mineralIndex].setPosition(position);
-			}
-				break;
-			}
-		}
-		else if ((player.type == ePlayerType::Human && line == Globals::TEXT_HEADER_PLAYER) ||
-			(player.type == ePlayerType::AI && line == Globals::TEXT_HEADER_PLAYERAI))
+		case eModelName::HQ:
+			player.HQ.setPosition(position);
+			break;
+		case eModelName::Mineral:
 		{
-			assert(!beginReadingFromFile);
-			beginReadingFromFile = true;
+			int mineralIndex = -1;
+			stream >> mineralIndex;
+			assert(mineralIndex >= 0 && mineralIndex < player.minerals.size());
+
+			player.minerals[mineralIndex].setPosition(position);
 		}
-	}
+		break;
+		}
+	};
+
+	ePlayerType playerType = player.type;
+	auto conditional = [playerType](const std::string& line)
+	{
+		return (playerType == ePlayerType::Human && line == Globals::TEXT_HEADER_PLAYER) ||
+			(playerType == ePlayerType::AI && line == Globals::TEXT_HEADER_PLAYERAI);
+	};
+
+	LevelFileHandler::loadFromFile(file, data, conditional);
 
 	return file;
 }
