52ccb296adc1efa0e74df1790cdcd4cc8e0ff650
Ryan Swann <RyanSwann1992@gmail.com>
2020-09-16 15:59:26 +0100

    Encapsulated data - made safer to use
    Setting target now done in one function call

--- RTSClone/RTSClone/UnitTarget.cpp
+++ RTSClone/RTSClone/UnitTarget.cpp
@@ -2,11 +2,32 @@
 #include "Globals.h"
 
 UnitTarget::UnitTarget()
-	: factionController(),
-	ID(Globals::INVALID_ENTITY_ID)
+	: m_factionController(),
+	m_ID(Globals::INVALID_ENTITY_ID)
 {}
 
 UnitTarget::UnitTarget(eFactionController factionController, int targetID)
-	: factionController(factionController),
-	ID(targetID)
+	: m_factionController(factionController),
+	m_ID(targetID)
 {}
+
+eFactionController UnitTarget::getFactionController() const
+{
+	return m_factionController;
+}
+
+int UnitTarget::getID() const
+{
+	return m_ID;
+}
+
+void UnitTarget::set(eFactionController factionController, int ID)
+{
+	m_factionController = factionController;
+	m_ID = ID;
+}
+
+void UnitTarget::reset()
+{
+	m_ID = Globals::INVALID_ENTITY_ID;
+}
--- RTSClone/RTSClone/UnitTarget.h
+++ RTSClone/RTSClone/UnitTarget.h
@@ -2,11 +2,19 @@
 
 #include "FactionController.h"
 
-struct UnitTarget
+class UnitTarget
 {
+public:
 	UnitTarget();
 	UnitTarget(eFactionController factionController, int targetID);
 
-	eFactionController factionController;
-	int ID;
+	eFactionController getFactionController() const;
+	int getID() const;
+
+	void set(eFactionController factionController, int ID);
+	void reset();
+
+private:
+	eFactionController m_factionController;
+	int m_ID;
 };
