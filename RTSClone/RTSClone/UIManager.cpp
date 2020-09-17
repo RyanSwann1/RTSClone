5fb4541b6fc70d128877b0e39b14c7c5d1691aba
Ryan Swann <RyanSwann1992@gmail.com>
2020-09-17 19:35:07 +0100

    Handling events of displaying Entity and clearing Entity window

--- RTSClone/RTSClone/UIManager.cpp
+++ RTSClone/RTSClone/UIManager.cpp
@@ -3,30 +3,117 @@
 #include "GameMessenger.h"
 #include "GameMessageType.h"
 #include <imgui/imgui.h>
+#include <string>
+
+namespace 
+{
+	const std::array<std::string, static_cast<size_t>(eEntityType::Max) + 1> ENTITY_NAME_CONVERSIONS =
+	{
+		"Unit", 
+		"Worker",
+		"HQ",
+		"SupplyDepot",
+		"Barracks",
+		"Mineral",
+		"Projectile"
+	};
+}
+
+//Widget
+Widget::Widget()
+	: active(false)
+{}
+
+PlayerDetailsWidget::PlayerDetailsWidget()
+	: Widget(),
+	resourcesAmount(0),
+	currentPopulation(0),
+	maxPopulation(0)
+{}
+
+void PlayerDetailsWidget::render()
+{
+	if (active)
+	{
+		ImGui::Begin("Player", nullptr);
+		ImGui::Text("Resources:");
+		ImGui::SameLine();
+		ImGui::Text(std::to_string(resourcesAmount).c_str());
+		ImGui::Text("Population:");
+		ImGui::SameLine();
+		ImGui::Text(std::to_string(currentPopulation).c_str());
+		ImGui::Text("Max Population:");
+		ImGui::SameLine();
+		ImGui::Text(std::to_string(maxPopulation).c_str());
+		
+		ImGui::End();
+
+		active = false;
+	}
+}
+
+EntityWidget::EntityWidget()
+	: Widget(),
+	entityType(),
+	entityHealth(0)
+{}
+
+void EntityWidget::render()
+{
+	if (active)
+	{
+		ImGui::Begin("Entity", nullptr);
+		ImGui::Text(ENTITY_NAME_CONVERSIONS[static_cast<int>(entityType)].c_str());
+		ImGui::SameLine();
+		ImGui::Text(std::to_string(entityHealth).c_str());
+		ImGui::End();
+	}
+}
 
 UIManager::UIManager()
+	: m_playerDetailsWidget(),
+	m_entityWidget()
 {
 	GameMessenger::getInstance().subscribe<GameMessages::UIDisplayPlayerDetails>([this](const GameMessages::UIDisplayPlayerDetails& gameMessage)
-		{return onDisplayResourceCount(gameMessage); }, this);
+		{return onDisplayPlayerDetails(gameMessage); }, this);
+
+	GameMessenger::getInstance().subscribe<GameMessages::UIDisplayEntity>([this](const GameMessages::UIDisplayEntity& gameMessage)
+		{ return onDisplayEntity(gameMessage); }, this);
+
+	GameMessenger::getInstance().subscribe<GameMessages::BaseMessage<eGameMessageType::UIClearDisplayEntity>>([this](
+		const GameMessages::BaseMessage<eGameMessageType::UIClearDisplayEntity>& gameMessage)
+		{return onClearDisplayEntity(gameMessage); }, this);
 }
 
 UIManager::~UIManager()
 {
 	GameMessenger::getInstance().unsubscribe<GameMessages::UIDisplayPlayerDetails>(this);
+	GameMessenger::getInstance().unsubscribe<GameMessages::UIDisplayEntity>(this);
+	GameMessenger::getInstance().unsubscribe<GameMessages::BaseMessage<eGameMessageType::UIClearDisplayEntity>>(this);
 }
 
-void UIManager::onDisplayResourceCount(const GameMessages::UIDisplayPlayerDetails& gameMessage)
+void UIManager::render()
 {
-	ImGui::SetNextWindowSize(ImVec2(350, 350), ImGuiCond_FirstUseEver);
-	if (ImGui::Begin("Player", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | 
-		ImGuiWindowFlags_NoFocusOnAppearing))
-	{
-		ImGui::Text("Resources:");
-		ImGui::Text(std::to_string(gameMessage.resourceAmount).c_str());
-		ImGui::Text("Population");
-		ImGui::Text(std::to_string(gameMessage.currentPopulationAmount).c_str());
-		ImGui::Text("Max Population");
-		ImGui::Text(std::to_string(gameMessage.maximumPopulationAmount).c_str());
-	}
-	ImGui::End();
+	m_playerDetailsWidget.render();
+	m_entityWidget.render();	
+}
+
+void UIManager::onDisplayPlayerDetails(const GameMessages::UIDisplayPlayerDetails& gameMessage)
+{
+	m_playerDetailsWidget.active = true;
+	m_playerDetailsWidget.resourcesAmount = gameMessage.resourceAmount;
+	m_playerDetailsWidget.currentPopulation = gameMessage.currentPopulationAmount;
+	m_playerDetailsWidget.maxPopulation = gameMessage.maximumPopulationAmount;
+}
+
+void UIManager::onDisplayEntity(const GameMessages::UIDisplayEntity& gameMessage)
+{
+	m_entityWidget.active = true;
+	m_entityWidget.entityType = gameMessage.entityType;
+	m_entityWidget.entityHealth = gameMessage.entityHealth;
+}
+
+void UIManager::onClearDisplayEntity(const GameMessages::BaseMessage<eGameMessageType::UIClearDisplayEntity>& gameMessage)
+{
+	m_entityWidget.active = false;
 }
--- RTSClone/RTSClone/UIManager.h
+++ RTSClone/RTSClone/UIManager.h
@@ -2,17 +2,58 @@
 
 #include "NonCopyable.h"
 #include "NonMovable.h"
+#include "EntityType.h"
+#include "GameMessageType.h"
 
 namespace GameMessages
 {
 	struct UIDisplayPlayerDetails;
+	struct UIDisplayEntity;
+	template <eGameMessageType type>
+	struct BaseMessage;
 };
+
+struct Widget
+{
+	Widget();
+
+	bool active;
+};
+
+struct PlayerDetailsWidget : public Widget
+{
+	PlayerDetailsWidget();
+	
+	void render();
+
+	int resourcesAmount;
+	int currentPopulation;
+	int maxPopulation;
+};
+
+struct EntityWidget : public Widget
+{
+	EntityWidget();
+
+	void render();
+
+	eEntityType entityType;
+	int entityHealth;
+};
+
 class UIManager : private NonCopyable, private NonMovable
 {
 public:
 	UIManager();
 	~UIManager();
 
+	void render();
+
 private:
-	void onDisplayResourceCount(const GameMessages::UIDisplayPlayerDetails& gameMessage);
+	PlayerDetailsWidget m_playerDetailsWidget;
+	EntityWidget m_entityWidget;
+
+	void onDisplayPlayerDetails(const GameMessages::UIDisplayPlayerDetails& gameMessage);
+	void onDisplayEntity(const GameMessages::UIDisplayEntity& gameMessage);
+	void onClearDisplayEntity(const GameMessages::BaseMessage<eGameMessageType::UIClearDisplayEntity>& gameMessage);
 };
