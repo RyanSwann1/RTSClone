f8389d1eac518d300da45c52bbcac0284ed85f14
Ryan Swann <RyanSwann1992@gmail.com>
2020-12-17 19:41:40 +0000

    Initial implementation of Health Bars

---
+++ RTSClone/RTSClone/HealthBarSprite.cpp
@@ -0,0 +1,92 @@
+#include "HealthBarSprite.h"
+#include "glad.h"
+#include "Globals.h"
+#include "Entity.h"
+#include <array>
+
+namespace
+{
+	const int DEFAULT_WIDTH = 50;
+	const int DEFAULT_HEIGHT = 10;
+	const int QUAD_VERTEX_COUNT = 6;
+
+	std::array<glm::vec2, QUAD_VERTEX_COUNT> getQuadCoords(glm::vec2 position, float width, float height)
+	{
+		return
+		{
+			glm::vec2(position.x, position.y),
+			glm::vec2(position.x + width, position.y),
+			glm::vec2(position.x + width, position.y + height),
+			glm::vec2(position.x + width, position.y + height),
+			glm::vec2(position.x, position.y + height),
+			glm::vec2(position.x, position.y)
+		};
+	};
+}
+
+HealthBarSprite::HealthBarSprite()
+	: m_vaoID(Globals::INVALID_OPENGL_ID),
+	m_vboID(Globals::INVALID_OPENGL_ID)
+{
+	glGenVertexArrays(1, &m_vaoID);
+	glGenBuffers(1, &m_vboID);
+}
+
+HealthBarSprite::HealthBarSprite(HealthBarSprite&& rhs) noexcept
+	: m_vaoID(rhs.m_vaoID),
+	m_vboID(rhs.m_vboID)
+{
+	rhs.m_vaoID = Globals::INVALID_OPENGL_ID;
+	rhs.m_vboID = Globals::INVALID_OPENGL_ID;
+}
+
+HealthBarSprite& HealthBarSprite::operator=(HealthBarSprite&& rhs) noexcept
+{
+	assert(this != &rhs);
+	if (this != &rhs)
+	{
+		onDestroy();
+
+		m_vaoID = rhs.m_vaoID;
+		m_vboID = rhs.m_vboID;
+
+		rhs.m_vaoID = Globals::INVALID_OPENGL_ID;
+		rhs.m_vboID = Globals::INVALID_OPENGL_ID;
+	}
+
+	return *this;
+}
+
+HealthBarSprite::~HealthBarSprite()
+{
+	onDestroy();
+}
+
+void HealthBarSprite::render(glm::vec2 position, glm::uvec2 windowSize) const
+{
+	float width = (static_cast<float>(DEFAULT_WIDTH) / windowSize.x) * 2.0f;
+	float height = (static_cast<float>(DEFAULT_HEIGHT) / windowSize.y) * 2.0f;
+	std::array<glm::vec2, QUAD_VERTEX_COUNT> quad = getQuadCoords(position, width, height);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_vboID);
+	glBufferData(GL_ARRAY_BUFFER, quad.size() * sizeof(glm::vec2), quad.data(), GL_STATIC_DRAW);
+
+	glBindVertexArray(m_vaoID);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (const void*)0);
+
+	glDrawArrays(GL_TRIANGLES, 0, QUAD_VERTEX_COUNT);
+}
+
+void HealthBarSprite::onDestroy()
+{
+	if (m_vaoID != Globals::INVALID_OPENGL_ID && m_vboID != Globals::INVALID_OPENGL_ID)
+	{
+		glDeleteVertexArrays(1, &m_vaoID);
+		glDeleteBuffers(1, &m_vboID);
+	}
+	else
+	{
+		assert(m_vaoID == Globals::INVALID_OPENGL_ID && m_vboID == Globals::INVALID_OPENGL_ID);
+	}
+}
---
+++ RTSClone/RTSClone/HealthBarSprite.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "NonCopyable.h"
+#include "glm/glm.hpp"
+
+class HealthBarSprite : private NonCopyable
+{
+public:
+	HealthBarSprite();
+	HealthBarSprite(HealthBarSprite&&) noexcept;
+	HealthBarSprite& operator=(HealthBarSprite&&) noexcept;
+	~HealthBarSprite();
+
+	void render(glm::vec2 position, glm::uvec2 windowSize) const;
+
+private:
+	unsigned int m_vaoID;
+	unsigned int m_vboID;
+
+	void onDestroy();
+};
