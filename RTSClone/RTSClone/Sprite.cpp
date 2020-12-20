169499ea490955c6a3a7549a8490d2e8006f8cac
Ryan Swann <RyanSwann1992@gmail.com>
2020-12-20 18:31:17 +0000

    Removed commented code

--- RTSClone/RTSClone/Sprite.cpp
+++ RTSClone/RTSClone/Sprite.cpp
@@ -84,63 +84,7 @@ void Sprite::render(const glm::vec3& position, glm::uvec2 windowSize, float widt
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (const void*)0);
 
 	glDrawArrays(GL_TRIANGLES, 0, QUAD_VERTEX_COUNT);
-
-	//float width = 0.0f;
-	//float height = DEFAULT_HEALTH_BAR_HEIGHT / windowSize.y * 2.0f;
-	//float yOffset = 0.0f;
-	//float currentHealth = static_cast<float>(m_health) / static_cast<float>(m_maximumHealth);
-	//switch (getEntityType())
-	//{
-	//case eEntityType::Unit:
-	//	width = Globals::UNIT_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = UNIT_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//case eEntityType::Worker:
-	//	width = Globals::WORKER_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = WORKER_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//case eEntityType::HQ:
-	//	width = Globals::HQ_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = HQ_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//case eEntityType::SupplyDepot:
-	//	width = Globals::SUPPLY_DEPOT_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = SUPPLY_DEPOT_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//case eEntityType::Barracks:
-	//	width = Globals::BARRACKS_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = BARRACKS_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//case eEntityType::Turret:
-	//	width = Globals::TURRET_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = TURRET_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//case eEntityType::Laboratory:
-	//	width = Globals::LABORATORY_STAT_BAR_WIDTH * currentHealth / windowSize.x * 2.0f;
-	//	yOffset = LABORATORY_HEALTH_BAR_YOFFSET / windowSize.y * 2.0f;
-	//	break;
-	//default:
-	//	assert(false);
-	//}
 }
-
-//void Sprite::render(glm::vec2 position, glm::uvec2 windowSize, float width, float height, float yOffset) const
-//{ 
-//	std::array<glm::vec2, QUAD_VERTEX_COUNT> quad = getQuadCoords(
-//		position, 
-//		width, 
-//		height,
-//		yOffset);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, m_vboID);
-//	glBufferData(GL_ARRAY_BUFFER, quad.size() * sizeof(glm::vec2), quad.data(), GL_STATIC_DRAW);
-//
-//	glBindVertexArray(m_vaoID);
-//	glEnableVertexAttribArray(0);
-//	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (const void*)0);
-//
-//	glDrawArrays(GL_TRIANGLES, 0, QUAD_VERTEX_COUNT);
-//}
 
 void Sprite::onDestroy()
 {
