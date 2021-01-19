9e8f3fa3d5ee5bb853221f4cd7dc316c8116b7c0
Ryan Swann <RyanSwann1992@gmail.com>
2021-01-19 07:12:03 +0000

    Added GameObject

---
+++ RTSClone/LevelEditor/GameObject.cpp
@@ -0,0 +1,114 @@
+#include "GameObject.h"
+#include "UniqueEntityIDDistributer.h"
+#include "ShaderHandler.h"
+#include "Globals.h"
+#include "Model.h"
+
+GameObject::GameObject(const Model& model, const glm::vec3& startingPosition, glm::vec3 startingRotation)
+	: m_position(startingPosition),
+	m_rotation(startingRotation),
+	m_AABB(),
+	m_ID(UniqueEntityIDDistributer::getInstance().getUniqueEntityID()),
+	m_model(model),
+	m_selected(false)
+{
+	m_AABB.reset(m_position, m_model);
+}
+
+GameObject::GameObject(GameObject&& rhs) noexcept
+	: m_position(rhs.m_position),
+	m_rotation(rhs.m_rotation),
+	m_AABB(std::move(rhs.m_AABB)),
+	m_ID(rhs.m_ID),
+	m_model(rhs.m_model),
+	m_selected(rhs.m_selected)
+{
+	rhs.m_ID = Globals::INVALID_ENTITY_ID;
+}
+
+GameObject& GameObject::operator=(GameObject&& rhs) noexcept
+{
+	assert(this != &rhs);
+	m_position = rhs.m_position;
+	m_rotation = rhs.m_rotation;
+	m_AABB = std::move(rhs.m_AABB);
+	m_ID = rhs.m_ID;
+	m_model = rhs.m_model;
+	m_selected = rhs.m_selected;
+
+	rhs.m_ID = Globals::INVALID_ENTITY_ID;
+
+	return *this;
+}
+
+glm::vec3& GameObject::getRotation()
+{
+	return m_rotation;
+}
+
+glm::vec3& GameObject::getPosition()
+{
+	return m_position;
+}
+
+const Model& GameObject::getModel() const
+{
+	return m_model;
+}
+
+void GameObject::setPosition(const glm::vec3& position)
+{
+	m_position = position;
+}
+
+void GameObject::setRotation(const glm::vec3 rotation)
+{
+	m_rotation = rotation;
+}
+
+void GameObject::resetAABB()
+{
+	m_AABB.reset();
+}
+
+int GameObject::getID() const
+{
+	return m_ID;
+}
+
+const glm::vec3& GameObject::getRotation() const
+{
+	return m_rotation;
+}
+
+const glm::vec3& GameObject::getPosition() const
+{
+	return m_position;
+}
+
+const AABB& GameObject::getAABB() const
+{
+	return m_AABB;
+}
+
+bool GameObject::isSelected() const
+{
+	return m_selected;
+}
+
+void GameObject::setSelected(bool selected)
+{
+	m_selected = selected;
+}
+
+void GameObject::render(ShaderHandler& shaderHandler) const
+{
+	m_model.get().render(shaderHandler, m_position, m_rotation);
+}
+
+#ifdef RENDER_AABB
+void GameObject::renderAABB(ShaderHandler& shaderHandler)
+{
+	m_AABB.render(shaderHandler);
+}
+#endif // RENDER_AABB
---
+++ RTSClone/LevelEditor/GameObject.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "NonCopyable.h"
+#include "AABB.h"
+#include <functional>
+
+class ShaderHandler;
+struct Model;
+class GameObject : private NonCopyable
+{
+public:
+	GameObject(const Model& model, const glm::vec3& startingPosition, glm::vec3 startingRotation = glm::vec3(0.0f));
+	GameObject(GameObject&&) noexcept;
+	GameObject& operator=(GameObject&&) noexcept;
+
+	glm::vec3& getRotation();
+	glm::vec3& getPosition();
+	const Model& getModel() const;
+	int getID() const;
+	const glm::vec3& getRotation() const;
+	const glm::vec3& getPosition() const;
+	const AABB& getAABB() const;
+	bool isSelected() const;
+
+	void setSelected(bool selected);
+	void setPosition(const glm::vec3& position);
+	void setRotation(const glm::vec3 rotation);
+
+	void render(ShaderHandler& shaderHandler) const;
+	void resetAABB();
+
+#ifdef RENDER_AABB
+	void renderAABB(ShaderHandler& shaderHandler);
+#endif // RENDER_AABB
+
+private:
+	glm::vec3 m_position;
+	glm::vec3 m_rotation;
+	AABB m_AABB;
+	int m_ID;
+	std::reference_wrapper<const Model> m_model;
+	bool m_selected;
+};
