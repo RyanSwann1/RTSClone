5d9b577d42a42165f33da502429b2c2296c1a583
Ryan Swann <RyanSwann1992@gmail.com>
2021-02-23 22:19:14 +0000

    New file - OpenGLResourceID
    Handles the RAII behind OpenGL resources. Also now movable so users can just call std::move() for interaction

---
+++ RTSClone/RTSClone/OpenGLResourceID.h
@@ -0,0 +1,33 @@
+#pragma once
+
+//glGenBuffers
+struct OpenGLResourceBufferID
+{
+	OpenGLResourceBufferID();
+	OpenGLResourceBufferID(const OpenGLResourceBufferID&) = delete;
+	OpenGLResourceBufferID& operator=(const OpenGLResourceBufferID&) = delete;
+	OpenGLResourceBufferID(OpenGLResourceBufferID&&) noexcept;
+	OpenGLResourceBufferID& operator=(OpenGLResourceBufferID&&) noexcept;
+	~OpenGLResourceBufferID();
+
+	unsigned int ID;
+
+private:
+	void onDestroy();
+};
+
+//glGenVertexArrays
+struct OpenGLResourceVertexArrayID : private NonCopyable
+{
+	OpenGLResourceVertexArrayID();
+	OpenGLResourceVertexArrayID(const OpenGLResourceVertexArrayID&) = delete;
+	OpenGLResourceVertexArrayID& operator=(const OpenGLResourceVertexArrayID&) = delete;
+	OpenGLResourceVertexArrayID(OpenGLResourceVertexArrayID&&) noexcept;
+	OpenGLResourceVertexArrayID& operator=(OpenGLResourceVertexArrayID&&) noexcept;
+	~OpenGLResourceVertexArrayID();
+
+	unsigned int ID;
+
+private:
+	void onDestroy();
+};
