922f7b6b907554861bec9d094bc45077fa1bad64
Ryan Swann <RyanSwann1992@gmail.com>
2021-04-12 21:34:01 +0100

    Added AIOccupiedBase. AI can now keep track of what Bases have.
    IE workers and will add other info

---
+++ RTSClone/RTSClone/AIOccupiedBases.cpp
@@ -0,0 +1,94 @@
+#include "AIOccupiedBases.h"
+#include "Worker.h"
+#include "Base.h"
+#include "Headquarters.h"
+#include "Globals.h"
+#include <assert.h>
+
+//AIOccupiedBase
+AIOccupiedBase::AIOccupiedBase(const Base& base)
+	: base(base),
+	workers()
+{}
+
+void AIOccupiedBase::addWorker(const Worker& worker)
+{
+	assert(std::find_if(workers.cbegin(), workers.cend(), [&worker](const auto& i)
+	{
+		return i.get().getID() == worker.getID();
+	}) == workers.cend());
+	workers.emplace_back(worker);
+}
+
+void AIOccupiedBase::removeWorker(const Worker& worker)
+{
+	auto iter = std::find_if(workers.begin(), workers.end(), [&worker](const auto& i)
+	{
+		return i.get().getID() == worker.getID();
+	});
+	assert(iter != workers.end());
+	workers.erase(iter);
+}
+
+//AIOccupiedBases
+AIOccupiedBases::AIOccupiedBases(const BaseHandler& baseHandler)
+	: m_bases()
+{
+	m_bases.reserve(baseHandler.getBases().size());
+}
+
+const std::vector<AIOccupiedBase>& AIOccupiedBases::getSortedBases(const glm::vec3& position)
+{
+	std::sort(m_bases.begin(), m_bases.end(), [&position](const auto& a, const auto& b)
+		{ return Globals::getSqrDistance(a.base.get().getCenteredPosition(), position) < 
+			Globals::getSqrDistance(b.base.get().getCenteredPosition(), position); });
+
+	return m_bases;
+}
+
+void AIOccupiedBases::addBase(const Base& base)
+{
+	assert(std::find_if(m_bases.cbegin(), m_bases.cend(), [&base](const auto& existingBase)
+	{
+		return existingBase.base.get().getCenteredPosition() == base.getCenteredPosition();
+	}) == m_bases.cend());
+	m_bases.emplace_back(base);
+}
+
+void AIOccupiedBases::removeBase(const Base& base)
+{
+	auto iter = std::find_if(m_bases.begin(), m_bases.end(), [&base](const auto& existingBase)
+	{
+		return existingBase.base.get().getCenteredPosition() == base.getCenteredPosition();
+	});
+	assert(iter != m_bases.end());
+	m_bases.erase(iter);
+}
+
+void AIOccupiedBases::addWorker(const Worker& worker, const Headquarters& headquarters)
+{
+	auto base = std::find_if(m_bases.begin(), m_bases.end(), [&headquarters](const auto& base)
+	{
+		return base.base.get().getCenteredPosition() == headquarters.getPosition();
+	});
+	assert(base != m_bases.end());
+
+	base->addWorker(worker);
+}
+
+void AIOccupiedBases::removeWorker(const Worker& worker)
+{
+	for (auto& base : m_bases)
+	{
+		for (auto iter = base.workers.begin(); iter != base.workers.end(); ++iter)
+		{
+			if (iter->get().getID() == worker.getID())
+			{
+				base.workers.erase(iter);
+				return;
+			}
+		}
+	}
+
+	assert(false);
+}
---
+++ RTSClone/RTSClone/AIOccupiedBases.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <functional>
+#include <vector>
+
+struct Base;
+class Worker;
+struct AIOccupiedBase
+{
+	AIOccupiedBase(const Base& base);
+
+	void addWorker(const Worker& worker);
+	void removeWorker(const Worker& worker);
+
+	std::reference_wrapper<const Base> base;
+	std::vector<std::reference_wrapper<const Worker>> workers;
+};
+
+class Headquarters;
+class BaseHandler;
+class AIOccupiedBases
+{
+public:
+	AIOccupiedBases(const BaseHandler& baseHandler);
+	AIOccupiedBases(const AIOccupiedBases&) = delete;
+	AIOccupiedBases& operator=(const AIOccupiedBases&) = delete;
+	AIOccupiedBases(AIOccupiedBases&&) = delete;
+	AIOccupiedBases& operator=(AIOccupiedBases&&) = delete;
+
+	const std::vector<AIOccupiedBase>& getSortedBases(const glm::vec3& position);
+	void addBase(const Base& base);
+	void removeBase(const Base& base);
+
+	void addWorker(const Worker& worker, const Headquarters& headquarters);
+	void removeWorker(const Worker& worker);
+
+private:
+	std::vector<AIOccupiedBase> m_bases;
+};
