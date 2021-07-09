1131f8f60846d03894c2f121be174a645d7478bf
Ryan Swann <RyanSwann1992@gmail.com>
2021-07-09 23:15:34 +0100

    GameObject now stored inside GameObjectManager file

--- RTSClone/LevelEditor/GameObjectManager.cpp
+++ RTSClone/LevelEditor/GameObjectManager.cpp
@@ -7,6 +7,44 @@
 #include <fstream>
 #include <sstream>
 
+//GameObject
+GameObject::GameObject(Model& model)
+	: position(),
+	rotation(),
+	aabb(),
+	model(model)
+{
+	aabb.reset(position, model);
+}
+
+GameObject::GameObject(Model& model, const glm::vec3& startingPosition, glm::vec3 startingRotation)
+	: position(startingPosition),
+	rotation(startingRotation),
+	aabb(),
+	model(model)
+{
+	aabb.reset(position, model);
+}
+
+void GameObject::setPosition(const glm::vec3& _position)
+{
+	position = _position;
+	aabb.update(position);
+}
+
+void GameObject::render(ShaderHandler& shaderHandler, bool highlight) const
+{
+	model.get().render(shaderHandler, position, rotation);
+}
+
+#ifdef RENDER_AABB
+void GameObject::renderAABB(ShaderHandler& shaderHandler)
+{
+	aabb.render(shaderHandler);
+}
+#endif // RENDER_AABB
+
+//GameObjectManager
 GameObjectManager::GameObjectManager()
 	: m_gameObjects()
 {}
