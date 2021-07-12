7b2483539da47f11c2381b8ac7e13e00abfe77b3
Ryan Swann <RyanSwann1992@gmail.com>
2021-07-12 19:49:59 +0100

    Revert "GameObject now stored inside GameObjectManager file"
    This reverts commit 1131f8f60846d03894c2f121be174a645d7478bf.

---
+++ RTSClone/LevelEditor/GameObject.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "AABB.h"
+#include <functional>
+
+class ShaderHandler;
+struct Model;
+struct GameObject
+{
+	GameObject(Model& model);
+	GameObject(Model& model, const glm::vec3& startingPosition, glm::vec3 startingRotation = glm::vec3(0.0f));
+	GameObject(const GameObject&) = delete;
+	GameObject& operator=(const GameObject&) = delete;
+	GameObject(GameObject&&) = default;
+	GameObject& operator=(GameObject&&) = default;
+
+	void setPosition(const glm::vec3& position);
+	void render(ShaderHandler& shaderHandler, bool highlight = false) const;
+
+#ifdef RENDER_AABB
+	void renderAABB(ShaderHandler& shaderHandler);
+#endif // RENDER_AABB
+
+	glm::vec3 position;
+	glm::vec3 rotation;
+	AABB aabb;
+	std::reference_wrapper<Model> model;
+};
