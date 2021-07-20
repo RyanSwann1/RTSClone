bf7161e6165a649e3477d6ade42c3dd3a4312edb
Ryan Swann <RyanSwann1992@gmail.com>
2021-07-20 20:14:51 +0100

    RenderPathMesh renamed to RenderPrimitiveMesh
    Added new functionality

---
+++ RTSClone/Shared/RenderPrimitiveMesh.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <array>
+#include <vector>
+
+struct Mesh;
+class AABB;
+class ShaderHandler;
+namespace RenderPrimitiveMesh
+{
+	void generate(const std::vector<glm::vec3>& path, Mesh& mesh);
+	void generate(AABB& aabb);
+	void render(ShaderHandler& shaderHandler, const glm::vec3& position, Mesh& mesh);
+	void render(ShaderHandler& shaderHandler, const std::vector<glm::vec3>& pathToPosition, Mesh& renderPathMesh);
+	void render(ShaderHandler& shaderHandler, AABB& aabb, const glm::vec3& colour, float opacity);
+}
